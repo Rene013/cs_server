2676f8890313c6c5bcb03c1e52ea6fc9b2481cb4
Ncjfcj Developer <123428042+Rene013@users.noreply.github.com>
2024-08-22 11:20:14 -0700

    Update process.php

--- app/public/auth.api/process.php
+++ app/public/auth.api/process.php
@@ -161,10 +161,10 @@ function email($to, $pin, $name)
     try{
         $mail->SMTPDebug = false; // SMTP::DEBUG_SERVER;                      
             $mail->isSMTP();                                            
-            $mail->Host       = 'access.ncjfcj.org';                     
+            $mail->Host       = '*******************';                     
             $mail->SMTPAuth   = true;                                   
-            $mail->Username   = 'authentication@access.ncjfcj.org';         //SMTP username
-            $mail->Password   = 'Ncjfcj@DotOrg2023!';             //SMTP password
+            $mail->Username   = '**************************';         //SMTP username
+            $mail->Password   = '********************';             //SMTP password
             $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            
             $mail->Port       = 465;                                    
 
